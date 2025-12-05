9759e6ab2daf88304f2b92f74c9ad0e2d72a2385
Ryan Restivo <ryan.restivo@gmail.com>
2025-12-04 19:43:12 -0500

    remove prints

--- biographies.py
+++ biographies.py
@@ -284,7 +284,7 @@ def merge_bio_create(person, biography, merging_bio):
               llm_data = {}
     keys_to_check = ['biography']
     if all(key in llm_data for key in keys_to_check):
-        #print("All required keys are present.")
+        #print("All required keys are present.") 
         return llm_data['biography']
     else:
         #print("Not all required keys are present.")
